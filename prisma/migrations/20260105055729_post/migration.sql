53eacc8817c50524410d5fb78dac568d05a96378
Noornabi <noornabimd9891@gmail.com>
2026-01-05 12:32:19 +0600

    Create post part

--- prisma/migrations/20260105055729_post/migration.sql
+++ prisma/migrations/20260105055729_post/migration.sql
@@ -1,24 +1,8 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "PostStatus" AS ENUM ('DRAFT', 'PUBLISHED', 'ARCHIVED');
 
 -- CreateEnum
 CREATE TYPE "CommentStatus" AS ENUM ('APPROVED', 'REJECT');
-
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- DropTable
-DROP TABLE "Post";
-
--- DropTable
-DROP TABLE "User";
 
 -- CreateTable
 CREATE TABLE "posts" (
--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
+import { postServices } from "./posts.services";
 
-const createPost = async (req : Request, res:Response) => {
-    res.send("Create a new post");
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const result = await postServices.createPost(req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      message: error instanceof Error ? error.message : "Post creation failed",
+    });
+  }
 };
 
 export const postController = {
-    createPost,
-    
-}
+  createPost,
+};
